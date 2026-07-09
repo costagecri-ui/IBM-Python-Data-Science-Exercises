a8a7860d8959bc6df154651c684ba67eee849872
costagecri-ui <costagecri@gmail.com>
2026-07-09 16:47:33 +0000

    Data_type_exercises

---
+++ data_type_exercises.py
@@ -0,0 +1,22 @@
+#data type of the result of: 6/2?
+print(type(6/2))
+#data type of the result of: 6/2?
+print(type(6/2))
+
+#data type of the result of 6//2?
+print(type(6//2))
+
+#data type of the result of "micro_rna"
+print(type("micro_rna"))
+
+#data type of the result of "micro"=="rna"
+print(type("micro"=="rna"))
+
+#converting the following number representing employeeid "184109843" to an integer
+print(int("184109843"))
+
+#converting this number representing financial value "184109.84356" to a floating point number
+print(float("184109.84356"))
+
+#converting this number to a string: 2944-434-2-22235-6654-7
+print(str("2944-434-2-22235-6654-7"))
